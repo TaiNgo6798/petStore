88a0ccb9c46d279f058fb774fe2778d3046ec878
Vũ Tấn Đạt <33850332+datvu1061998@users.noreply.github.com>
2019-11-10 00:06:41 +0700

    update lock and role account

--- backEnd/server.js
+++ backEnd/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type,	Authorization"
